56790f96218be131cbc42df7a1c591d5cd886161
Piterskuy <piterskuy@bk.ru>
2015-09-14 21:35:25 +0400

    HW1 final without optional

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,24 @@
+package ru.javawebinar.topjava.web;
+
+import ru.javawebinar.topjava.LoggerWrapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * User: Piterskuy
+ * Date: 14.09.2015
+ */
+public class MealServlet extends HttpServlet {
+    private static final LoggerWrapper LOG = LoggerWrapper.get(UserServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to mealList");
+
+//        request.getRequestDispatcher("/mealList.jsp").forward(request, response);
+        response.sendRedirect("mealList.jsp");
+    }
+}
---
+++ src/main/webapp/mealList.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: admin
+  Date: 14.09.2015
+  Time: 12:57
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Meal list</title>
+</head>
+<body>
+<h2><a href="${pageContext.request.contextPath}" target="_blank">>На заглавную</a></h2>
+
+<h2>Meal list</h2>
+</body>
+</html>
